3468db9f45556f9bced26767f0ea23d62ff8dfd5
Ali <alibegvivi@gmail.com>
2026-09-13 15:10:21 +0300

    Update proc_load_silver.sql

--- cripts/silver/proc_load_silver.sql
+++ cripts/silver/proc_load_silver.sql
@@ -66,3 +66,41 @@ SELECT
 		ELSE bronze.crm_prd_info.prd_end_dt
 		END AS prd_end_dt
 FROM bronze.crm_prd_info;
+
+--SALES DETAILS
+INSERT INTO silver.crm_sales_details( 
+	sls_ord_num,
+	sls_prd_key,
+	sls_cust_id,
+	sls_order_dt,
+	sls_ship_dT,
+	sls_due_dt,
+	sls_sales,
+	sls_quantity,
+	sls_price
+)
+
+SELECT
+	sls_ord_num,
+	sls_prd_key,
+	sls_cust_id,
+	-- чем LENGTH(CAST(sls_ship_dt AS VARCHAR)!=8 я использовал FLOOR(LOG(sls_order_dt))+1!=8 т.к. быстрее
+	CASE WHEN sls_order_dt=0 OR FLOOR(LOG(sls_order_dt))+1!=8 THEN NULL
+	 	 ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
+	END AS sls_order_dt,
+	CASE WHEN sls_ship_dt=0 OR FLOOR(LOG(sls_ship_dt))+1!=8 THEN NULL
+	 	 ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
+	END AS sls_ship_dt,
+	CASE WHEN sls_due_dt=0 OR FLOOR(LOG(sls_due_dt))+1!=8 THEN NULL
+	 	 ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
+	END AS sls_due_dt,
+	CASE WHEN sls_sales IS NULL OR sls_sales<=0 OR sls_sales!=sls_quantity*ABS(sls_price) THEN sls_quantity*sls_price
+		 ELSE sls_sales
+	END AS sls_sales,
+	sls_quantity,
+	CASE WHEN sls_price<0 THEN ABS(sls_price) 
+		 WHEN sls_price IS NULL OR sls_price=0 THEN sls_sales/sls_quantity
+		 ELSE sls_price
+	END AS sls_price
+FROM bronze.crm_sales_details
+;
